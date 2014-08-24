7b122f57487c3ad9d66e5186ec720b13be51a07d
Matthew Schoolmaster <schoolmaster@gmail.com>
2014-08-24 15:04:20 -0600

    namespace dalli expires

--- app/controllers/peek/expires_controller.rb
+++ app/controllers/peek/expires_controller.rb
@@ -2,9 +2,17 @@ module Peek
     class ExpiresController < ApplicationController
       before_filter :restrict_non_access
 
-      def expire
+      def dalli_expire
         @cache_key = CGI.unescape(params[:cache_key])
-        success = Rails.cache.delete @cache_key
+
+        # if you're using race_condition_ttl in you cache requests, just expire the key
+        # if you're not and want the cache expired immediately, delete the cache object
+        #cached_object = Rails.cache.read @cache_key
+        #Rails.cache.delete @cache_key
+        #success = Rails.cache.write @cache_key, cached_object, expires_in: 0.seconds, race_condition_ttl: 2.minutes
+        #success = Rails.cache.delete @cache_key
+        success = Rails.cache.expire @cache_key
+
         respond_to do |format|
           if success
             format.js { render 'success' }
